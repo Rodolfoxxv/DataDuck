e6cdcd55ee718b2ae322f0eafabe7e8439e9ec0f
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-13 02:00:46 -0300

    one up docker

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+Este é um módulo de exemplo que usa Streamlit para exibir "Hello World!".
+"""
+
+import streamlit as st
+
+def main():
+    """
+    Esta função escreve "Hello World!" usando Streamlit.
+    """
+    st.write("Hello Word!")
+
+if __name__ == "__main__":
+    main()
